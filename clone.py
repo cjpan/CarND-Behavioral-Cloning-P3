9634d1303733e1475a77969f0ce9a8540bafaee5
cjpan <chengjie.pan@gmail.com>
2017-03-27 21:40:43 +0800

    initial training code file and saved model file.

---
+++ clone.py
@@ -0,0 +1,61 @@
+import csv
+import cv2
+import numpy as np
+
+lines = []
+
+with open('./data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+lines = lines[1:]
+
+images = []
+measurements = []
+for line in lines:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    #print(filename)
+    current_path = './data/IMG/' + filename
+    img = cv2.imread(current_path)
+    images.append(img)
+
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_measurements.append(measurement * -1.0)
+
+
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
+print("data ready")
+
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPool2D
+
+model = Sequential()
+model.add(Lambda(lambda x: (x / 255.) - 0.5, input_shape=(160,320,3)))
+model.add(Convolution2D(6, 5, 5, activation="relu"))
+model.add(MaxPool2D())
+model.add(Convolution2D(6, 5, 5, activation="relu"))
+model.add(MaxPool2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
+model.add(Dense(1))
+print("layer ready")
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
+
+model.save('model.h5')
