3be45bc922d2bbb944a0cc2223c5b7a11b6e8e14
cjpan <chengjie.pan@gmail.com>
2017-03-28 11:17:16 +0800

    - Crop the image by 50, 20, 0, 0. - fix MaxPooling2D and Conv2D. - fix batch_size to 128.

--- clone.py
+++ clone.py
@@ -39,16 +39,17 @@ print("data ready")
 
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
-from keras.layers.convolutional import Convolution2D
-from keras.layers.pooling import MaxPool2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add(Lambda(lambda x: (x / 255.) - 0.5, input_shape=(160,320,3)))
-model.add(Convolution2D(6, 5, 5, activation="relu"))
-model.add(MaxPool2D())
-model.add(Convolution2D(6, 5, 5, activation="relu"))
-model.add(MaxPool2D())
+model.add(Cropping2D(cropping=((50,20),(0,0)), input_shape=(160,320,3)))
+model.add(Lambda(lambda x: (x / 255.) - 0.5))
+model.add(Conv2D(6, (5, 5), activation="relu"))
+model.add(MaxPooling2D())
+model.add(Conv2D(6, (5, 5), activation="relu"))
+model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(120))
 model.add(Dense(84))
@@ -56,6 +57,6 @@ model.add(Dense(1))
 print("layer ready")
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5, batch_size=128)
 
 model.save('model.h5')
