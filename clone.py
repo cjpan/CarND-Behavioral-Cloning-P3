5183d2d22e253e4ae7581af6f77fb6e0a6b2090d
cjpan <chengjie.pan@gmail.com>
2017-04-01 15:22:08 +0800

    - code change for fix_generator

--- clone.py
+++ clone.py
@@ -2,61 +2,81 @@ import csv
 import cv2
 import numpy as np
 
-lines = []
+samples = []
 
 with open('./data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
-        lines.append(line)
+        samples.append(line)
 
-lines = lines[1:]
+samples = samples[1:]
 
-images = []
-measurements = []
-for line in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    #print(filename)
-    current_path = './data/IMG/' + filename
-    img = cv2.imread(current_path)
-    images.append(img)
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+from sklearn.utils import shuffle
 
-    measurement = float(line[3])
-    measurements.append(measurement)
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1:
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset : offset+batch_size]
 
-augmented_images, augmented_measurements = [], []
-for image, measurement in zip(images, measurements):
-    augmented_images.append(image)
-    augmented_measurements.append(measurement)
-    augmented_images.append(cv2.flip(image, 1))
-    augmented_measurements.append(measurement * -1.0)
+            images = []
+            measurements = []
+            for sample in batch_samples:
+                filename = sample[0].split('/')[-1]
+                current_path = './data/IMG/' + filename
+                center_img = cv2.imread(current_path)
+                center_angle = float(sample[3])
+                images.append(center_img)
+                measurements.append(center_angle)
 
+            augmented_images, augmented_measurements = [], []
+            for image, measurement in zip(images, measurements):
+                augmented_images.append(image)
+                augmented_measurements.append(measurement)
+                augmented_images.append(cv2.flip(image, 1))
+                augmented_measurements.append(measurement * -1.0)
 
+            X_train = np.array(augmented_images)
+            y_train = np.array(augmented_measurements)
+            yield shuffle(X_train, y_train)
 
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
-print("data ready")
+#X_train = np.array(augmented_images)
+#y_train = np.array(augmented_measurements)
+#print("data ready")
 
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Conv2D
-from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Cropping2D(cropping=((50,20),(0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: (x / 255.) - 0.5))
-model.add(Conv2D(6, (5, 5), activation="relu"))
-model.add(MaxPooling2D())
-model.add(Conv2D(6, (5, 5), activation="relu"))
-model.add(MaxPooling2D())
+model.add(Conv2D(24, (5, 5), strides=(2,2), activation="relu"))
+model.add(Conv2D(36, (5, 5), strides=(2,2), activation="relu"))
+model.add(Conv2D(48, (5, 5), strides=(2,2), activation="relu"))
+model.add(Conv2D(64, (3, 3), activation="relu"))
+model.add(Conv2D(64, (3, 3), activation="relu"))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 print("layer ready")
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5, batch_size=128)
+model.fit_generator(train_generator,
+                    steps_per_epoch=len(train_samples),
+                    validation_data=validation_generator,
+                    validation_steps=len(validation_samples),
+                    epochs=3)
 
 model.save('model.h5')
+
+#predict = model.predict(X_train, batch_size=128)
+#print(predict)
